2ffc29cb7c72ca44f30930aee85d0c04e30dc282
Jubayer husain <jubayerhusai360@gmail.com>
2024-11-02 10:15:59 +0600

    footer section  done

---
+++ src/componants/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Banner() {
+    return (
+        <div>
+            this is a banner section
+        </div>
+    )
+}
+
+export default Banner
--- src/componants/Layout/Layout.jsx
+++ src/componants/Layout/Layout.jsx
@@ -1,10 +1,12 @@
 
 import Navber from './../Navber/Navber';
+import Footer from './../Footer/Footer';
 function Layout() {
     return (
         <div>
             <Navber />
             this is a main layout container
+            <Footer />
         </div>
     )
 }
